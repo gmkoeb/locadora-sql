cca5e97bb620d399b36aaa539cd80c2c74b161f0
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-10-14 14:52:07 -0300

    Include search query for customers with more than 2 locations.

--- Customers/searchCustomersTable.sql
+++ Customers/searchCustomersTable.sql
@@ -1,2 +1,11 @@
 -- SQLite
 SELECT NAME, LASTNAME, EMAIL FROM CUSTOMERS WHERE STATE = 'SP'
+
+SELECT * FROM CUSTOMERS
+JOIN (
+    SELECT CUSTOMER_ID, COUNT(*) as number_of_locations
+    FROM LOCATIONS
+    GROUP BY CUSTOMER_ID
+    HAVING number_of_locations >= 2
+) AS CustomersWithMoreThanTwoLocations
+ON CUSTOMERS.id = CustomersWithMoreThanTwoLocations.CUSTOMER_ID
