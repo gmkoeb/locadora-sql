dac4798e7a158a439d5becb263b3829c912e8a1a
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-10-14 10:59:28 -0300

    Seed customers table with customers

---
+++ seedCustomersTable.sql
@@ -0,0 +1,13 @@
+-- SQLite
+
+INSERT INTO CUSTOMERS (NAME, LASTNAME, EMAIL, ADDRESS, CITY, STATE, BIRTH_DATE, PHONE) 
+VALUES ('Roberto', 'Silva', 'roberto@gmail.com', 'Rua José, 32', 'São Roque', 'SP', '1987-04-25', '(11) 964'),
+       ('Maria', 'Gomes', 'maria@yahoo.com', 'Rua das Flores, 44', 'São Paulo', 'SP', '1988-03-24', '(11) 974'), 
+       ('Renata', 'Antunes', 'renata@hotmail.com', 'Rua dos Antunes, 55', 'Sorocaba', 'SP', '1970-05-01', '(11) 974'),
+       ('Josefa', 'Duarte', 'josefa20@gmail.com', 'Rua dos Flocos, 387', 'Mairinque', 'SP', '1988-06-18', '(11) 973'), 
+       ('André', 'Linares', 'andre.lin@gmail.com', 'Rua Palmeiras, 3', 'Rio de Janeiro', 'RJ', '2000-11-04', '(11) 975'),
+       ('Mário', 'Santana', 'mario@gmail.com', 'Rua das Rosas, 8', 'Salvador', 'BA', '1999-12-15', '(11) 974'),
+       ('Luis', 'Duarte', 'luis@hotmail.com', 'Rua das Orquidias, 18', 'Fortaleza', 'CE', '1985-07-06', '(11) 987'),
+       ('Dalva', 'Malheiros', 'dalva@gmail.com', 'Rua das Nogueiras, 12', 'São Paulo', 'SP', '1956-08-03', '(11) 966'),
+       ('Carolina', 'Medeiros', 'carol@ig.com.br', 'Rua dos Jatobás, 1', 'Cajamar', 'SP', '1974-08-21', '(11) 935'),
+       ('Marcos', 'Rodrigues', 'marcos@uol.com.br', 'Rua das Amazonas, 89', 'Recife', 'PE', '1966-05-25', '(11) 972')
